54e974cd491b156c5e67af458e0ff89dbd1b68fe
AslanMus <asmus01@mail.ru>
2024-06-09 13:47:53 +0300

    Ok

--- src/test/java/tests/ReqresInLombokTest.java
+++ src/test/java/tests/ReqresInLombokTest.java
@@ -4,6 +4,7 @@ import model.lombok.LoginBodyLombokModel;
 import model.lombok.LoginResponseLombokModel;
 import org.junit.jupiter.api.Test;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -44,6 +45,29 @@ public class ReqresInLombokTest {
                 .log().headers()
                 .log().body()
                 .filter(new AllureRestAssured())
+
+                .contentType(JSON)
+                .body(loginBody)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModel.class);
+
+        assertThat(loginResponse.getToken()).isEqualTo("QpwL5tke4Pnpja7X4");
+    }
+    @Test
+    void loginWithCustomListenerTest() {
+        LoginBodyLombokModel loginBody = new LoginBodyLombokModel();
+        loginBody.setEmail("eve.holt@reqres.in");
+        loginBody.setPassword("cityslicka");
+
+        LoginResponseLombokModel loginResponse = given()
+                .filter(withCustomTemplates())
+                .log().uri()
+                .log().headers()
                 .log().body()
                 .contentType(JSON)
                 .body(loginBody)
